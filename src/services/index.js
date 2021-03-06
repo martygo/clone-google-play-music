a52449faf7341a012a5992f249a1463c50de62bf
Martins Gouveia <gouveiamartins6@gmail.com>
2021-03-06 22:30:11 +0100

    remove api key

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 	baseURL: `https://api.napster.com/v2.2/`,
-	headers: { apikey: "YzYzMTQ0NGMtODY2Ny00MzNhLThhYTYtYTY0ZmRlMTAxN2Qw" },
+	headers: { apikey: "aqui vai a sua api do napster " },
 });
 
 export default api;
