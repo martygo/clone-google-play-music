c83647d22c926ff34afeac49ba2b4970a746d872
Martins Gouveia <gouveiamartins6@gmail.com>
2022-10-19 00:48:15 +0100

    🔐 Add api-key public

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 	baseURL: `https://api.napster.com/v2.2/`,
-	headers: { apikey: "aqui vai a sua api do napster " },
+	headers: { apikey: "ZjdjZmYyYzQtMDRmOC00MGZmLWJiZjMtNjk5MTk2ZTIyN2Ni" },
 });
 
 export default api;
